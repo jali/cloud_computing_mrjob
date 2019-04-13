14e0413d7d7acd6d8ee92abd93f7838335715d08
Jalal Maqdisi <jalal.maqdisi@gmail.com>
2019-04-13 18:28:16 +0100

    add shortjokes and count neighbours

---
+++ ipynb_map_reduce_cc1_top10.py
@@ -0,0 +1,25 @@
+"""The classic MapReduce job: count the frequency of words.
+"""
+from mrjob.job import MRJob
+import re
+
+WORD_RE = re.compile(r"[\w']+")
+ALPHABET_RE = re.compile(r"[a-zA-Z]")
+
+class MRCC1TOP10(MRJob):
+    
+    def mapper(self, _, line):
+        the_line = WORD_RE.findall(line)
+        for key, value in enumerate(the_line):
+            if (value == "my" and (key + 1 < len(the_line))):
+                yield (value + "," + the_line[key+1].lower(), 1)
+
+    def combiner(self, value, counts):
+        yield (value, sum(counts))
+
+    def reducer(self, value, counts):
+        yield (value, sum(counts))
+        
+
+if __name__ == '__main__':
+    MRCC1TOP10.run()
